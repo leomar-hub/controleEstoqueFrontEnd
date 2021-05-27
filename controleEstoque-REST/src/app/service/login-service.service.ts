d21b4e5eed971950e549f00d9a42fd125476bbeb
Sergio Leomar <sergio.dias2@fatec.sp.gov.br>
2021-05-27 02:17:59 -0300

    Retorno do token no frontend OK (console)

--- controleEstoque-REST/src/app/service/login-service.service.ts
+++ controleEstoque-REST/src/app/service/login-service.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { AppConstants } from '../app-constants';
+import { error } from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +11,20 @@ export class LoginServiceService {
   constructor(private http: HttpClient) { }
 
   login(usuario){
-    return this.http.post(AppConstant.baseLogin, JSON.stringify(usuario)).subscribe(data =>{
+    return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario)).subscribe(data =>{
       /*Retorno Http */
 
-      console.info(JSON.parse (JSON.stringify(data)));
-    })
+      var token = (JSON.parse (JSON.stringify(data)).Authorization.split(' ')[1]);
+
+      localStorage.setItem("token", token);
+
+      console.info("Token: " + localStorage.getItem("token"));
+
+    },
+      error =>{
+        console.error("Erro ao fazer login");
+      }
+    )
   }
 
 }
